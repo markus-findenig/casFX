d6dd394f5c266b3705f454e517fee705a2af9036
Markus Findenig <mfindeni@edu.aau.at>
2015-01-28 15:25:10 +0100

    d

--- casFX/src/view/SimulatorView.java
+++ casFX/src/view/SimulatorView.java
@@ -1,19 +1,11 @@
 package view;
 
-import java.awt.Button;
-import java.awt.Insets;
-import java.awt.Label;
-import java.awt.TextField;
 
-import javax.swing.*;
-
-import javafx.application.Application;
 import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
-import javafx.scene.*;
+
 
 public class SimulatorView {
 	
