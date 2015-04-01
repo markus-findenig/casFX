b7747e4fd4c094feb83ce5e74d670c20f4819922
Markus Findenig <mfindeni@edu.aau.at>
2015-04-01 11:08:46 +0200

    Wifi PSK

---
+++ casFX/src/tests/Wifi_PSK_Generator.java
@@ -0,0 +1,40 @@
+package tests;
+
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+
+import javax.crypto.*;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+
+public class Wifi_PSK_Generator {
+
+	public static void main(String[] args) throws InvalidKeySpecException, NoSuchAlgorithmException {
+
+		String ssid = "zero";
+		String passphrase = "Ud8w3rB7a2";
+
+		//String Key = PBKDF2(passphrase, ssid, 4096, 256);
+		
+		SecretKeyFactory f = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+		char[] pass = passphrase.toCharArray();
+		byte[] salt = ssid.getBytes();
+		KeySpec ks = new PBEKeySpec(pass ,salt, 4096, 256);
+		SecretKey s = f.generateSecret(ks);
+		Key k = new SecretKeySpec(s.getEncoded(),"AES");
+		
+		System.out.println("SSID = " + ssid );
+		System.out.println("Passphrase = " + passphrase );
+		
+		System.out.print("PSK = ");
+		for (int j = 0; j < k.getEncoded().length; j++) {
+			System.out.format("%02x", k.getEncoded()[j]);
+		}
+		System.out.println();
+				
+		
+	}
+
+}
